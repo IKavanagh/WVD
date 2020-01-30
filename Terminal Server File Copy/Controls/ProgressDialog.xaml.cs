4b6ed3759ff3fed129be0a8277b25f9bc048e067
Ian Kavanagh <ian.kavanagh@azyra.com>
2020-01-30 17:15:03 +0000

    Update namespace and add ProgressDialog user control

---
+++ Terminal Server File Copy/Controls/ProgressDialog.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TerminalServerFileCopy.Controls
+{
+	public partial class ProgressDialog : UserControl
+	{
+		public ProgressDialog()
+		{
+			InitializeComponent();
+		}
+
+		public event EventHandler<EventArgs> Cancelled;
+
+		private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			Cancelled?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
